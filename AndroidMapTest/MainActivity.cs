3f6feebe7b5075f132972c05cdccfea432e5a04d
zhangzhimin <zhangzhimin@gdepb.gov.cn>
2013-12-17 17:18:54 +0800

    test ground overlay.

--- AndroidMapTest/MainActivity.cs
+++ AndroidMapTest/MainActivity.cs
@@ -3,6 +3,8 @@ using Android.OS;
 using Com.Baidu.Mapapi;
 using Com.Baidu.Mapapi.Map;
 using Com.Baidu.Platform.Comapi.Basestruct;
+using Android.Graphics.Drawables;
+using Android.Graphics;
 
 namespace BaiduMapTest {
 
@@ -17,18 +19,32 @@ namespace BaiduMapTest {
 			base.OnCreate(savedInstanceState);
 
 			mapManager = new BMapManager(this.Application);
-			mapManager.Init("", null);
+			mapManager.Init(GetString(Resource.String.baidu_map_api_key), null);
 
 			// Set our view from the "main" layout resource
 			SetContentView(Resource.Layout.Main);
 
 			mapView = FindViewById<MapView>(Resource.Id.mapview);
 			mapView.SetBuiltInZoomControls(true);
+			mapView.Satellite = true;
 
 			var mapController = mapView.Controller;
 			var point = new GeoPoint((int)(39.915 * 1E6), (int)(116.404 * 1E6));
 			mapController.SetCenter(point);
 			mapController.SetZoom(12);
+			addGroundOverlay();
+		}
+
+		private void addGroundOverlay() {
+			GroundOverlay overlay = new GroundOverlay(mapView);
+			GeoPoint leftBottom = new GeoPoint((int)(39.815 * 1E6), (int)(116.304 * 1E6));
+			GeoPoint rightUp = new GeoPoint((int)(39.999 * 1E6), (int)(116.504 * 1E6));
+			Drawable d = Resources.GetDrawable(Resource.Drawable.Icon);
+			Bitmap bitmap = ((BitmapDrawable)d).Bitmap;
+			Ground ground = new Ground(bitmap, leftBottom, rightUp);
+
+			mapView.Overlays.Add(overlay);
+			overlay.AddGround(ground);
 		}
 
 		protected override void OnDestroy() {
